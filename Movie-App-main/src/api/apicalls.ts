c90c4afe04e9738b9512fc217b42d396c3dd8b69
Ayush Kumar <142109210+codebreaker3008@users.noreply.github.com>
2024-07-31 12:17:17 +0530

    Movie

---
+++ Movie-App-main/src/api/apicalls.ts
@@ -0,0 +1,16 @@
+const apikey: string = '7e7626e5e32f0f2c0872916216c5f2f0';
+export const baseImagePath = (size: string, path: string) => {
+  return `https://image.tmdb.org/t/p/${size}${path}`;
+};
+export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}`;
+export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}`;
+export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`;
+export const searchMovies = (keyword: string) => {
+  return `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}`;
+};
+export const movieDetails = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}`;
+};
+export const movieCastDetails = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apikey}`;
+};
